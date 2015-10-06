1979a99c37bcd8e8bae15e55e8ccdace7ca2acee
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-10-05 22:48:40 -0700

    Add latest regexps

--- vm-shared/calculator.php
+++ vm-shared/calculator.php
@@ -45,15 +45,27 @@
                     if (empty($expr)) {
                         echo "";
                     }
+
                     // Quick regexp reference found here:
                     // https://secure.php.net/manual/en/function.preg-match.php
-                    // Invalid expression if anything but digits and operators.
+
+                    // Invalid expression if anything but digits, operators, and periods.
                     elseif (preg_match("/[^0-9\/\*\+\-\. ]/i", $expr)) {
                         echo "Invalid expression. Please try again.";
                     }
 
+                    // Invalid if space before decimal point 
+                    elseif (preg_match("/\s\./i", $expr)) {
+                        echo "Invalid expression. Please try again.";
+                    }
+
+                    // Invalid if double decimal points
+                    elseif (preg_match("/\.\./i", $expr)) {
+
+                    }
+
                     // TRY ALL IN DEMO
-                    // TODO: Catch divide by 0 exception
+                    // TODO: Catch divide by 0 exception. Could do clever regexp instead.
                     // TODO: Catch --- or ++ or ** or // (BUT -- allowed?)
                     // TODO: Double-check Piazza for other cases
                     // TODO: Double decimal points
@@ -61,7 +73,14 @@
                     // TODO: Space after decimal point
 
                     else {
-                        eval("\$answer = $expr;");
+                        try {
+                            // Try @ sign to suppress error, then use get_last_error()
+                            // to see if we get divide by zero message
+                            eval("\$answer = $expr;");
+                        }
+                        catch (Exception $e) {
+                            echo 'Error details: ' . $e->getMessage() . "\n";
+                        }
                         echo $expr . " = " . $answer; 
                     }
                 ?>
