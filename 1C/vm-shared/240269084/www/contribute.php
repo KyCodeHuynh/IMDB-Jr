2651a8931481dfb5f1153ccaefb7ca2106f73dd3
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-10-30 18:08:56 -0700

    Set-up first form-group for 143 project

--- 1C/vm-shared/240269084/www/contribute.php
+++ 1C/vm-shared/240269084/www/contribute.php
@@ -12,8 +12,8 @@
     <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.00"/>
 
     <!-- CSS stylesheets -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min  .css">
-    <link rel="stylesheet" type="text/css" href="css/styles.css"
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 
     <!-- Database set-up -->
     <?php
@@ -27,22 +27,70 @@
       // TODO: Switch to "CS143" for production!
       // Select which database we'll use
       // Specifying a link identifier lets it know which link to use
-      mysql_select_db("CS143", $db_connect)
+      mysql_select_db("TEST", $db_connect)
      ?>
   </head>
 
   <!-- BODY -->
   <body>
-  <!-- TODO: Add in fields for other info besides name -->
-    <!-- TODO: Replace with radio button to select actor/director -->
+    <h1>Add Person</h1>
     <div class="container">
-      <h1>Add Actor</h1>
-        <form method="GET">
-          <textarea name="actor-name" cols="100" rows="1"></textarea>
-          <br/>
+      <form method="GET">
+        <!-- Fieldset just groups together related form data -->
+        <fieldset>
+          <legend>Actor/director information</legend>
+          <!-- Names -->
+          <span>
+            First name:
+            <input type="text" name="first_name">
+            Last name:
+            <input type="text" name="last_name">
+          </span>
+          <!-- DOB and DOD -->
+          <br>
+          <span>
+            Date of birth:
+            <input type="text" name="birth_date" placeholder="YYYYMMDD">
+            Date of death:
+            <input type="text" name="death_date" placeholder="NULL if N/A">
+          </span>
+          <!-- Actor or director? -->
+          <br>
+          <span>
+            <input type="radio" name="person_type" value="actor" checked>Actor
+            <input type="radio" name="person_type" value="director">Director 
+          </span>
+          <!-- Female or male? -->
+          <br>
+          <span>
+            <input type="radio" name="sex" value="female" checked>Female
+            <input type="radio" name="sex" value="male">Male
+          </span>
+          <br>
           <input type="submit" value="Submit">
-        </form>
+        </fieldset>
+      </form>
     </div>
+
+    <?php 
+      // Having a link identifier lets MySQL determine the character set to use
+      // See: http://bit.ly/1isaeWS
+      $first_name = mysql_real_escape_string($_GET['first_name'], $db_connect);
+      $last_name = mysql_real_escape_string($_GET['last_name'], $db_connect);
+      $birth_date = mysql_real_escape_string($_GET['birth_date'], $db_connect);
+      $death_date = mysql_real_escape_string($_GET['death_date'], $db_connect);
+      $person_type = mysql_real_escape_string($_GET['person_type']), $db_connect);
+      $sex = mysql_real_escape_string($_GET['sex']), $db_connect);
+
+      // TODO: Handle missing field input
+      // if (empty($query)) {
+      //   echo "All fields are required. Please try again."
+      // }
+      // else
+
+
+      // TODO: Need to get and update MaxPersonID too
+     ?>
 
     <div class="container">
       <h1>Add Director</h1>
@@ -55,17 +103,18 @@
 
     <!-- TODO: Movie info -->
     <!-- TODO: Movie comments -->
+    <!-- TODO: Movie reviews require a TIMESTAMP -->
     <!-- TODO: Actor to movie relation -->
     <!-- TODO: Director to movie relation -->
-    
+
     <!-- Never forget to close an opened resource -->
     <?php 
       mysql_close($db_connect);
      ?>
 
     <!-- JavaScript -->
-    <script type="text/javascript" src="./js/jquery-2.1.4.min.js"></script>
-    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
   </body>
 </html>
 
