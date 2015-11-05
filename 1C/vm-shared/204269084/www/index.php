57e6e81f4d8dc644272489b4955bbfa117cd8575
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-11-04 20:55:12 -0800

    Corrections to Selenium test cases and revamp front page with nav bar.

--- 1C/vm-shared/204269084/www/index.php
+++ 1C/vm-shared/204269084/www/index.php
@@ -29,23 +29,28 @@
   </head>
 
   <body>
-      <div class="row">
-          <h1>Welcome to IMDB Jr.!</h1>
-      </div>
-
-      <div class="row">
-        <!-- TODO: Introduce a snazzy navbar! -->
-        <ul>
-          <li><a href="search.php">Search</a></li>
-          <li><a href="showMovie.php?mid=3641">Show Movies</a></li>
-          <li><a href="showActor.php?aid=3035">Show Actors</a></li>
-          <li><a href="addPerson.php">Add an Actor or Director</a></li>
-          <li><a href="addMovie.php">Add a Movie</a></li>
-          <li><a href="addComments.php">Add Comments About a Movie</a></li>
-          <li><a href="addDirector2Movie.php">Associate a Director with a Movie</a></li>
-          <li><a href="addActor2Movie.php">Associate an Actor with a Movie</a></li>
+    <div class="row">
+      <div class="large-12 large-centered columns">
+        <ul class="button-group">
+          <li><a class="button" href="search.php">Search</a></li>
+          <li><a class="button" href="showMovie.php">Show Movies</a></li>
+          <li><a class="button" href="showActor.php">Show Actors</a></li>
+          <li><a class="button" href="addPerson.php">Add an Actor or Director</a></li>
+          <li><a class="button" href="addMovie.php">Add a Movie</a></li>
+          <li><a class="button" href="addComments.php">Add Comments About a Movie</a></li>
+          <li><a class="button" href="addDirector2Movie.php">Associate a Director with a Movie</a></li>
+          <li><a class="button" href="addActor2Movie.php">Associate an Actor with a Movie</a></li>
         </ul>
+      </div>
+    </div>
 
+
+
+    <div class="row">
+      <div class="large-7 large-centered columns">
+        <div class="row">
+          <h1>Welcome to IMDB Jr.!</h1>
+        </div>
         <p>Welcome to our Internet Movie Database! <br>
           Feel free to look around at some of your favorite films and actors! <br> 
           Who knows, maybe you'll be surprised by what you find...
@@ -58,48 +63,7 @@
           </div>
         </p>
       </div>
-
-<!--       <div class="row">
-        <div class="large-12 columns">
-          <h2>Top 5 Rated Movies</h2>
-          <?php 
-            // A query is normally formed from GET'ed form
-            // We sanitized it in all cases against malicious SQL queries
-            $query = "SELECT * FROM Review LIMIT 5";
-            $sanitized = mysql_real_escape_string($query, $db_connect);
-            $results = mysql_query($sanitized, $db_connect);
-
-            // Print out results
-            print "<table>";
-
-            // Get the number of columns
-            $num_fields = mysql_num_fields($results);
-
-            // First row is the names of the fields
-            print "<tr>";
-            for ($j = 0; $j < $num_fields; $j++) {
-              // Args: results, field-offset
-              $num_fields = mysql_field_name($results, $j);
-              print "<th>$fieldname</th>";
-            }
-            print "</tr>";
-
-            // Print remaining rows
-            while ($row = mysql_fetch_row($results)) {
-              print "<tr>";
-              // Print all columns within a row
-              for ($i = 0; $i < $num_fields; $i++) {
-                $item = $row[$i];
-                print "<td>$item</td>";
-              }
-              print "</tr>";
-            }
-
-            print "</table>";
-           ?>
-          </div>
-        </div>
-      </div> -->
+    </div>
     
     <!-- Never forget to close an opened resource -->
     <?php 
